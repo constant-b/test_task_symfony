a0bcf6edebd13e5d15232840de42618a2ccf0659
constant-b <kostik.butenko@gmail.com>
2021-08-10 19:06:43 +0300

    [+] Lucky Controller

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController
+{
+    /**
+     * @Route("/lucky/number")
+     * @return Response
+     * @throws \Exception
+     */
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response("<html><body>Lucky number is: " . $number . "</body></html>");
+    }
+}
